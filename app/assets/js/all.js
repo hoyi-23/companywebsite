ffcb158cda09a3dc6f2cd0bfabaa1bdda3a6de36
“hoyi” <keira.yen0715@gmail.com>
2021-07-31 21:45:33 +0800

    add product page

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,24 @@
+//test
 console.log('Hello!');
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
 });
+
+
+//nav&tab
+const nav = document.querySelectorAll('.nav-item');
+nav.forEach(item => item.addEventListener('click',function(e){  
+  nav.forEach(item => item.classList.remove('active'));        
+  item.classList.add('active')  
+  const panes = document.querySelectorAll('.product__tab');  
+  panes.forEach(pane => {
+    const tab = pane.dataset.tab;
+    if(item.id == tab){      
+      panes.forEach(pane => pane.classList.remove('show'));
+      pane.classList.add('show');
+    }    
+  })    
+}))
+
+
