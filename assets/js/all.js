eaa24ba49918a9842b141727fa19a0215ed04914
“hoyi” <keira.yen0715@gmail.com>
2021-07-31 21:45:09 +0800

    Update 2021-07-31T13:45:07.114Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,29 @@
 "use strict";
 
+//test
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+}); //nav&tab
+
+var nav = document.querySelectorAll('.nav-item');
+nav.forEach(function (item) {
+  return item.addEventListener('click', function (e) {
+    nav.forEach(function (item) {
+      return item.classList.remove('active');
+    });
+    item.classList.add('active');
+    var panes = document.querySelectorAll('.product__tab');
+    panes.forEach(function (pane) {
+      var tab = pane.dataset.tab;
+
+      if (item.id == tab) {
+        panes.forEach(function (pane) {
+          return pane.classList.remove('show');
+        });
+        pane.classList.add('show');
+      }
+    });
+  });
 });
 //# sourceMappingURL=all.js.map
